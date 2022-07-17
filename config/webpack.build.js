093f8e75c3024be24db57563900a31d52fb23df4
wangshuyan <wangshuyan@cmhi.chinamobile.com>
2022-07-17 20:04:29 +0800

    path set

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -1,9 +1,6 @@
 const path = require('path');
 module.exports = function webpackBuild (config) {
-    const { version, projectPath, projectName, versionBasicPath } = config;
-
     return {
-        mode: 'production',
-       
+        mode: 'production'
     };
 };
